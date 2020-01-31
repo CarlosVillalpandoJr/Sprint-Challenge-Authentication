692f893a1d2c554e08dab8799644938e2fec87c8
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-01-31 10:32:13 -0600

    register endpoint working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -4,16 +4,24 @@ const jwt = require('jsonwebtoken');
 
 const { jwtSecret } = require('../config/secret.js');
 
+const Users = require('../users/users-model.js');
+
 router.post('/register', (req, res) => {
   let user = req.body;
-  const hash = bcrypt.hashSync(user.password, 10);
+  const hash = bcrypt.hashSync(user.password, 6);
   user.password = hash;
 
-  
+  Users.add(user)
+    .then(savedUser => {
+      res.status(201).json(savedUser);
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  
 });
 
 module.exports = router;
