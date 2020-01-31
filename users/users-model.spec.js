88bbe94a421504d0a9119eedf782676fdb2daf1e
CarlosVillalpandoJr <carlosvillalpandojr21@gmail.com>
2020-01-31 11:24:20 -0600

    user-model testing complete

--- users/users-model.spec.js
+++ users/users-model.spec.js
@@ -9,4 +9,19 @@ describe('users model', function() {
             expect(process.env.DB_ENV).toBe('testing');
         })
     })
+
+    describe('add()', function() {
+        beforeEach(async () => {
+            await db('users').truncate();
+        })
+
+        it('adds new user to the database', async function() {
+            await Users.add({ username: 'gigi', password: "gigi123"});
+            await Users.add({ username: 'canelo', password: 'canelo123'});
+
+            const users = await db('users');
+
+            expect(users).toHaveLength(2)
+        })
+    })
 })
